d21660658df39f32c651ee1a37e6be392dc911bf
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-06-23 11:09:29 +0200

    Sorted eigenvalues + normalized laplacian

--- mushroom/classify.m
+++ mushroom/classify.m
@@ -10,7 +10,7 @@ labels = categorical(labels{:,:}); % convert table to a vector of categorical va
 data(:,1) = []; 
 
 % Short the dataset for testing
-crop = 10;
+crop = 1000;
 cats = categorical(data{1:crop,:});
 
 % Convert table to matrix
@@ -24,20 +24,44 @@ numrows = size(cats,1);
 numcols = size(cats,2);
 sigma = 0.5;
 distance = pdist2(cats, cats, 'hamming');
-S = exp(-1 * distance/(2*sigma));  %pairwise hamming distances
+S = exp(-1 * distance/(2*(sigma^2)));  %pairwise hamming distances
 
-% TODO Hay que quitarle unos
+
 uns = ones(numrows,1);
+
+% Take off zeros from diagonal
+S = S - diag(uns);
 
 % Adjency matrix is semblança matrix
 D = diag(S * uns);
 
+% Laplacian
 L = D - S;
+
+% Normalized laplacian
+L_sn = D^(-0.5) * L * D^(-0.5);
+
+% Random walk laplacian
+L_rw = D^(-1)*L;
 
 % Calculate eigenvalues
 % [V,D] = eig(A) devuelve la matriz diagonal D de los valores propios y
 % la matriz V cuyas columnas son los vectores propios derechos correspondientes, de manera que A*V = V*D.
 [V,V_D]=eig(L);
+% De forma predeterminada, eig no siempre devuelve los valores propios
+% y los vectores propios en orden. Hay que ordenarlos
+[d,ind] = sort(diag(V_D));
+V_D_sort = V_D(ind,ind);
 
+[idx,C] = kmeans(V_D,2);
 
+% figure; 
+% plot(L_sn(idx==1,1),L_sn(idx==1,2),'r.','MarkerSize',12);
+% hold on
+% plot(L_sn(idx==2,1),L_sn(idx==2,2),'b.','MarkerSize',12)
+% plot(C(:,1),C(:,2),'kx', 'MarkerSize',15)
+% legend('Cluster 1','Cluster 2','Centroids','Location','NW')
+% hold off
+
+CM = confusionmat(L_sn, cats)
 
